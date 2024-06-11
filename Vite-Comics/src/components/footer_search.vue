d3f6db44c3ebf05f7157e625d590db26e8cea0b0
SimoneColombo1 <simcolombo2@gmail.com>
2024-06-11 23:52:54 +0200

    creata la struttura della navbar nel footer

--- Vite-Comics/src/components/footer_search.vue
+++ Vite-Comics/src/components/footer_search.vue
@@ -3,9 +3,63 @@
 </script>
 
 <template>
+<nav class="Search">
+    
+    <button class="sign-in">SIGN-UP NOW!</button>
+      
+    <ul class="socials">
+        <li>
+    <h2> FOLLOW US</h2> 
+        </li>
+        <li>
+            LOGO
+        </li>
+        <li>
+           LOGO 
+        </li>
+        <li>
+           LOGO 
+        </li>
+        <li>
+            LOGO
+        </li>
+        <li>
+            LOGO
+        </li>
+       </ul> 
 
+</nav>
 </template>
 
 <style scoped>
+.Search{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: #303030;
+    padding: 2rem;
+}
 
+.socials{
+    display: flex;
+    flex-direction: row;
+}
+.socials li{
+    list-style: none;
+    padding: 1rem;
+}
+.socials h2{
+    color: #0777df;
+}
+.Search button{
+    width: 8rem;
+    height: 3rem;
+    background-color: #303030;
+    border: solid 2px #0777df;
+    color: white;
+    
+}
+.Search :hover{
+    background-color: #0777df;
+}
 </style>
