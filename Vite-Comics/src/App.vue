a43193207ffb72ad716e9ce4e84680ee7ed91ae4
SimoneColombo1 <simcolombo2@gmail.com>
2024-06-11 22:23:12 +0200

    Creato l'header

--- Vite-Comics/src/App.vue
+++ Vite-Comics/src/App.vue
@@ -1,9 +1,23 @@
 <script>
+ import DcHeader from './components/header.vue';
+ import DcMain from './components/main.vue';
+ import PrimaryFooter from './components/primary_footer.vue';
+ import FooterSearch from './components/footer_search.vue';
+ import SecondaryFooter from './components/secondary_footer.vue';
+export default{
+    components:{
+   DcHeader,
+   DcMain,
+   PrimaryFooter,
+   FooterSearch,
+   SecondaryFooter
+  },
+}
 
 </script>
 
 <template>
-
+<DcHeader/>
 </template>
 
 <style scoped>
--- Vite-Comics/src/components/header.vue
+++ Vite-Comics/src/components/header.vue
@@ -3,9 +3,82 @@
 </script>
 
 <template>
-
+<header>
+    <div class="Header-container">
+        <div class="DC-Logo">DC</div>
+        <ul>
+            <li>
+               <a href="">       CHARACTERS</a>
+            </li>
+            <li class="active">
+             <a href="" >  COMICS</a>
+            </li>
+            <li>
+            <a href=""> MOVIES</a> 
+             </li>
+              <li>
+            <a href="">  TV </a>  
+            </li>
+              <li>
+           <a href="">  GAMES</a>
+              </li>
+             <li>
+           <a href="">COLLECTIBLES</a> 
+           </li>
+          <li>
+         <a href=""> VIDEOS</a>
+            </li>
+         <li>
+     <a href=""> FANS</a>
+         </li>
+         <li>
+        <a href="">NEWS</a>
+           </li>
+         <li>
+     <a href="">SHOP </a>
+        </li>
+        </ul>
+    </div>
+</header>
 </template>
 
 <style scoped>
+
+.Header-container{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: #ffffff;
+    padding: 2rem;
+}
+.Header-container ul{
+display: flex;
+flex-direction: row;
+
+}
+.Header-container li{
+list-style: none;
+margin: 0 1rem;
+padding: 1rem 0;
+}
+
+.Header-container a:hover{
+    color:#0282f9;
+}
+
+
+
+.Header-container a{
+    color:#58595d ;
+}
+.active{
+   
+    border-bottom: #0282f9 solid 3px;
+    
+}
+.active a{
+    color: #0282f9;
+}
+
 
 </style>
